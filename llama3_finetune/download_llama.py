fde289eaf83d7cd62e8053303ec1bf0fb2cbbe01
Pradeep Kadubandi <pkaduban@amazon.com>
2024-09-10 15:15:27 -0700

    Needed because default config of NousResaerch differs from our config. Without this code change, the model download will attempt to use the default config with rope scaling.

--- llama3_finetune/download_llama.py
+++ llama3_finetune/download_llama.py
@@ -3,10 +3,15 @@ import os
 from transformers.models.llama.modeling_llama import LlamaForCausalLM
 import datasets
 from pathlib import Path
+from huggingface_hub import snapshot_download
+import shutil
 
-save_dir="/shared/"
-model = LlamaForCausalLM.from_pretrained("NousResearch/Meta-Llama-3.1-8B")
-torch.save(model.state_dict(), os.path.combine(save_dir, "llama3.1-8b-hf-pretrained.pt"))
+save_dir="/shared"
+save_model_dir="/shared/llama-3.1"
+shutil.copy('config.json', os.path.join(save_model_dir, "config.json"))
+snapshot_download(repo_id="NousResearch/Meta-Llama-3.1-8B", allow_patterns=["*.safetensors", "*.json"], ignore_patterns="config.json", local_dir=save_model_dir)
+model = LlamaForCausalLM.from_pretrained(save_model_dir)
+torch.save(model.state_dict(), os.path.join(save_dir, "llama3.1-8b-hf-pretrained.pt"))
 
 #downloading dolly dataset
 dataset="databricks/databricks-dolly-15k" 
