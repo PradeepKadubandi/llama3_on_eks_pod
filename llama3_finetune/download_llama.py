8e2125709e927ac24db92ddf3b93f0f12eea810a
Pradeep Kadubandi <pkaduban@amazon.com>
2024-09-11 11:13:03 -0700

    Adding a step that was missing and was done manually.

--- llama3_finetune/download_llama.py
+++ llama3_finetune/download_llama.py
@@ -8,6 +8,7 @@ import shutil
 
 save_dir="/shared"
 save_model_dir="/shared/llama-3.1"
+os.makedirs(save_model_dir, exist_ok=True)
 shutil.copy('config.json', os.path.join(save_model_dir, "config.json"))
 snapshot_download(repo_id="NousResearch/Meta-Llama-3.1-8B", allow_patterns=["*.safetensors", "*.json"], ignore_patterns="config.json", local_dir=save_model_dir)
 model = LlamaForCausalLM.from_pretrained(save_model_dir)
